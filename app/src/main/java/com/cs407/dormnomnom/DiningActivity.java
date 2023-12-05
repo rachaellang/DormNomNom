370d02885763749b9b85de492dce72bfd02252d0
Ben <bklicker@wisc.edu>
2023-12-05 11:36:04 -0600

    Hall names are now passed between activities. All dining halls are now clickable.

--- app/src/main/java/com/cs407/dormnomnom/DiningActivity.java
+++ app/src/main/java/com/cs407/dormnomnom/DiningActivity.java
@@ -13,13 +13,28 @@ public class DiningActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dining);
 
-//        TODO: currently only for Gordon, apply to all dining halls based on API data
         ImageView gordonImageView = findViewById(R.id.gordon);
-        gordonImageView.setOnClickListener(v -> navigateToHallActivity());
+        gordonImageView.setOnClickListener(v -> navigateToHallActivity("gordon-avenue-market"));
+
+        ImageView rhetaImageView = findViewById((R.id.rheta));
+        rhetaImageView.setOnClickListener((v -> navigateToHallActivity("rhetas-market")));
+
+        ImageView fourLakesImageView = findViewById((R.id.fourlakes));
+        fourLakesImageView.setOnClickListener((v -> navigateToHallActivity("four-lakes-market")));
+
+        ImageView carsonsImageView = findViewById((R.id.carson));
+        carsonsImageView.setOnClickListener((v -> navigateToHallActivity("carsons-market")));
+
+        ImageView lizImageView = findViewById((R.id.liz));
+        lizImageView.setOnClickListener((v -> navigateToHallActivity("lizs-market")));
+
+        ImageView lowellImageView = findViewById((R.id.lowell));
+        lowellImageView.setOnClickListener((v -> navigateToHallActivity("lowell-market")));
     }
 
-    private void navigateToHallActivity() {
+    private void navigateToHallActivity(String hallName) {
         Intent intent = new Intent(DiningActivity.this, HallActivity.class);
+        intent.putExtra("HALL_NAME", hallName);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/cs407/dormnomnom/HallActivity.java
+++ app/src/main/java/com/cs407/dormnomnom/HallActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,11 @@ public class HallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hall);
+
+        String hallName = getIntent().getStringExtra("HALL_NAME");
+
+        TextView hallNameView = findViewById(R.id.diningHallName);
+        hallNameView.setText(hallName);
 
         Button myMealButton = findViewById(R.id.myMeal);
         myMealButton.setOnClickListener(v -> navigateToMealActivity());
