0b6258929ac73ff4cfabfd3c4c00962d3ce28639
klicker08 <124731966+klicker08@users.noreply.github.com>
2023-12-05 17:46:09 -0600

    Cleaned up requestTasks

--- app/src/main/java/com/cs407/dormnomnom/GetRequestTask.java
+++ app/src/main/java/com/cs407/dormnomnom/GetRequestTask.java
@@ -6,7 +6,6 @@ import org.json.JSONException;
 import java.util.ArrayList;
 
 public class GetRequestTask extends AsyncTask<String, Void, ArrayList<Station>> {
-    // This interface is used to send data back to the calling activity
     public interface AsyncResponse {
         void processFinish(ArrayList<Station> output);
     }
@@ -22,7 +21,6 @@ public class GetRequestTask extends AsyncTask<String, Void, ArrayList<Station>> 
         // Perform the network request in the background
         try {
             GetRequest request = new GetRequest(params[0], params[1], params[2], params[3], params[4]);
-            // Assuming GetRequest has a method to execute the request and return a result
             return request.getStations();
         } catch (JSONException e) {
             e.printStackTrace();
@@ -32,7 +30,6 @@ public class GetRequestTask extends AsyncTask<String, Void, ArrayList<Station>> 
 
     @Override
     protected void onPostExecute(ArrayList<Station> result) {
-        // This method is called on the main thread, process the result here
         delegate.processFinish(result);
     }
 }
