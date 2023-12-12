999be7bb16d2c327b916f23bd0334d101e0a0fc4
Ben <bklicker@wisc.edu>
2023-12-11 19:18:50 -0600

    Pushing GMS dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
