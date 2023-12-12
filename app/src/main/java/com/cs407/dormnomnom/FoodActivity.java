ad43e15a5d7ffd284ccc9d6e7fa45a83a93bde9b
Gabriel <barahonamald@wisc.edu>
2023-12-11 22:10:19 -0600

    Back button & My Meal button are now functional within FoodActivity.java

--- app/src/main/java/com/cs407/dormnomnom/FoodActivity.java
+++ app/src/main/java/com/cs407/dormnomnom/FoodActivity.java
@@ -2,6 +2,9 @@ package com.cs407.dormnomnom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +13,24 @@ import org.json.JSONObject;
 
 public class FoodActivity extends AppCompatActivity {
 
+    String hallName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
         Intent intent = getIntent();
-        String hallName = intent.getStringExtra("HALL_NAME");
+        hallName = intent.getStringExtra("HALL_NAME");
         String foodItemJson = intent.getStringExtra("FOOD_ITEM_JSON");
+
+        // handles Back button
+        ImageView backButton = findViewById(R.id.backFood);
+        backButton.setOnClickListener(v -> navigateToClass(new Intent (FoodActivity.this, HallActivity.class)));
+
+        // handles My Meal button
+        Button myMeal = findViewById(R.id.myMeal);
+        myMeal.setOnClickListener(v -> navigateToClass(new Intent(FoodActivity.this, MealActivity.class)));
 
         // Convert JSON string to FoodItem
         try {
@@ -31,10 +44,28 @@ public class FoodActivity extends AppCompatActivity {
                     json.getDouble("gProtein")
             );
 
+            // handles changing Food Name textView
+            TextView foodName = findViewById(R.id.foodName);
+            foodName.setText(selectedFoodItem.getName());
+
+            // displays carbs
+            TextView macroDisplay = findViewById(R.id.macroDisplay);
+            macroDisplay.setText("        Macronutrient Info\n\n" +
+                    "   Per serving\n" +
+                    "Calories: " + selectedFoodItem.getNutrition()[0] + "\n" +
+                    "Fat: " + selectedFoodItem.getNutrition()[1] + " g\n" +
+                    "Carbs: " + selectedFoodItem.getNutrition()[2] + " g\n" +
+                    "Sodium: " + selectedFoodItem.getNutrition()[3] + " mg\n" +
+                    "Protein: " + selectedFoodItem.getNutrition()[4] + " g");
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
-
+    private void navigateToClass(Intent intent) {
+        intent.putExtra("HALL_NAME", hallName);
+        startActivity(intent);
+        finish();
     }
 }
